2574cbcb2855426ab9e40ca19010a9c3d2b05e0c
Fuel_Mule <jude.scarbrough@gmail.com>
2025-01-28 12:37:40 -0500

    path error fixes

--- modelnode.py
+++ modelnode.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
+sys.path.append(parent_dir)
+
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import urllib.parse as urlparse
 import socket
